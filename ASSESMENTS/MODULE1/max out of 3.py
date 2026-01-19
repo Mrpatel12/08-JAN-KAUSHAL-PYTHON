9bcdaf73914afcdab28f38f4cad485e535b4a2d0
kaushal kapupara <kaushal120804@gmail.com>
2026-01-19 13:26:00 +0530

    first commit

---
+++ ASSESMENTS/MODULE1/max out of 3.py
@@ -0,0 +1,18 @@
+a=int(input("Enter a Value A :"))
+b=int(input("Enter a Value b :"))
+c=int(input("Enter a Value c :"))
+
+
+print("A",a)
+print("B",b)
+print("C",c)
+
+if a>=b and a>=c :
+    print("A is max")
+    
+elif b>=c and b>=a :
+       print("B is Max ")
+
+else :
+      print ("C is max")
+
