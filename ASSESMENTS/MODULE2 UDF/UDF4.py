ed95db40245091bca8ff6d73169caf3bc13cbdd5
kaushal kapupara <kaushal120804@gmail.com>
2026-02-11 11:29:01 +0530

    first commit

--- ASSESMENTS/MODULE2 UDF/UDF4.py
+++ ASSESMENTS/MODULE2 UDF/UDF4.py
@@ -1,36 +1,36 @@
-# UDF enter your id name city and store in the list
-# and print output in list
+# # UDF enter your id name city and store in the list
+# # and print output in list
 
 
 
-def getdata(data_list):
-     user_id = input("Enter your ID:")
-     user_name = input("Enter your name:")
-     user_city = input("Enter your city:")
+# def getdata(data_list): 
+#      user_id = input("Enter your ID:")
+#      user_name = input("Enter your name:")
+#      user_city = input("Enter your city:")
 
-     data_list 
-     {
-          "ID": user_id,
-          "Name":user_name,
-          "City":user_city
+#      data_list 
+#      {
+#           "ID": user_id,
+#           "Name":user_name,
+#           "City":user_city
 
-     }
+#      }
 
-     data_list.append(data_list)
-     print("Your data stored successfully.")
+#      data_list.append(data_list)
+#      print("Your data stored successfully.")
 
-     data_list = []
+#      data_list = []
 
-     while True:
+#      while True:
 
-        getdata(data_list)
-        other_user = input("If you want to add another" \
-                           "user (Yes/No)")
+#         getdata(data_list)
+#         other_user = input("If you want to add another" \
+#                            "user (Yes/No)")
         
-        if other_user != 'Yes':
-            break
+#         if other_user != 'Yes':
+#             break
 
-        print ("\n Whole record ----")
-        for user in data_list:
-          print(user)
+#         print ("\n Whole record ----")
+#         for user in data_list:
+#           print(user)
 
