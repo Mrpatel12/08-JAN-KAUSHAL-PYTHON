f9e8c72fcd255fe6d25116178068836f29d45b06
kaushal kapupara <kaushal120804@gmail.com>
2026-03-16 13:20:55 +0530

    first commit

---
+++ Assignements/Module-3_Database/dbapp.py
@@ -0,0 +1,25 @@
+import sqlite3
+
+try:
+    db=sqlite3.connect("demo.db")
+    print("Datbase craeted")
+except Exception as e:
+    print(e)
+
+table_create ="Create table poduct_master(id integer primary " \
+"key autoincrement,name text, city text)" 
+try:
+    db.execute(table_create)
+except Exception as e:
+    print(e)    
+
+insert_data ="Insert into poduct_master(name,city)values('Kaushal','Jamnagar'),('Raj','Rajkot)')" 
+try:
+    db.execute(insert_data)
+    db.commit()
+    print("Record insreted")
+except Exception as e:
+    print(e)    
+
+
+
