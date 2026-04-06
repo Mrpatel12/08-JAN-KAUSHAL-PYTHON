b5514827f2c506025e91b230121064a1999933a2
kaushal kapupara <kaushal120804@gmail.com>
2026-04-06 13:27:34 +0530

    first commit

---
+++ Projects/Task1/Task1/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for Task1 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Task1.settings')
+
+application = get_wsgi_application()
+
+def task1_wsgi():
